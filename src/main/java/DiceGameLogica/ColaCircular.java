ee5fabe6b12b7ab8cf1ac049fb7c0b4e01aa30b5
AlfredoSM225 <alfredo.santos25@uabc.edu.mx>
2026-03-20 14:28:16 -0700

    Se implementaron las partes de la ventana gráfica que faltaban y las tablas de estadísticas, para ello se tuvieron que modificar clases anteriores y algunos métodos. El programa está terminado

---
+++ src/main/java/DiceGameLogica/ColaCircular.java
@@ -0,0 +1,59 @@
+package DiceGameLogica;
+
+public class ColaCircular<T> {
+    private T[] colaC;
+    private int inicio;
+    private int fin;
+
+    public ColaCircular() {
+        colaC = (T[]) new Object[1000];
+        inicio = -1;
+        fin = -1;
+    }
+
+    public void insertarCircular(T dato) {
+        if ((fin == colaC.length - 1 && inicio == 0) || (fin + 1 == inicio)) {
+            System.out.println("Desbordamiento");
+            return;
+        }
+
+        if (fin == colaC.length - 1) {
+            fin = 0;
+        } else {
+            fin = fin + 1;
+        }
+
+        colaC[fin] = dato;
+
+        if (inicio == -1) {
+            inicio = 0;
+        }
+    }
+
+    public T eliminarCircular() {
+        if (inicio == -1) {
+            System.out.println("Subdesbordamiento");
+            return null;
+        }
+
+        T dato = colaC[inicio];
+
+        if (inicio == fin) {
+            inicio = -1;
+            fin = -1;
+        } else {
+            if (inicio == colaC.length - 1) {
+                inicio = 0;
+            } else {
+                inicio = inicio + 1;
+            }
+        }
+        return dato;
+    }
+
+    public int getTamano() {
+        if (inicio == -1) return 0;
+        if (fin >= inicio) return fin - inicio + 1;
+        return (colaC.length - inicio) + (fin + 1);
+    }
+}
---
+++ src/main/java/GUIDiceGame/Graficos.java
@@ -0,0 +1,150 @@
+package GUIDiceGame;
+
+import DiceGameLogica.Ficha;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+public class Graficos {
+    private GridPane contenedor;
+    private BarChart<String, Number> graficaSalidas;
+    private XYChart.Series<String, Number> serieSalidas;
+    private BarChart<String, Number> graficaTotales;
+    private XYChart.Series<String, Number> serieTotales;
+    private BarChart<String, Number> graficaTiempos;
+    private XYChart.Series<String, Number> serieTiempos;
+    private BarChart<String, Number> graficaActividad;
+    private ArrayList<XYChart.Series<String, Number>> todasSeries;
+    private ComboBox<String> selector;
+    private int turnos;
+
+    public Graficos(){
+        contenedor = new GridPane();
+        contenedor.setHgap(10);
+        contenedor.setVgap(10);
+        turnos = 0;
+
+        inicializarGraficaSalidas();
+        inicializarGraficaTotales();
+        inicializarGraficaTiempos();
+        inicializarGraficoActividad();
+        contenedor.add(graficaSalidas,0,1);
+        contenedor.add(graficaTotales,0,2);
+        contenedor.add(graficaTiempos, 1, 1);
+    }
+
+    public void inicializarGraficaSalidas(){
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("Número de Turno");
+        NumberAxis y = new NumberAxis(0, 70, 5);
+        y.setLabel("Fichas Salidas");
+
+        graficaSalidas = new BarChart<>(x, y);
+        graficaSalidas.setTitle("Salida de fichas");
+
+        serieSalidas = new XYChart.Series<>();
+        serieSalidas.setName("Número de fichas");
+        graficaSalidas.getData().add(serieSalidas);
+    }
+
+    public void inicializarGraficaTotales(){
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("Número de Turno");
+        NumberAxis y = new NumberAxis(0, 100, 10);
+        y.setLabel("Fichas en el sistema");
+
+        graficaTotales = new BarChart<>(x, y);
+        graficaTotales.setTitle("Fichas en sistema");
+
+        serieTotales = new XYChart.Series<>();
+        serieTotales.setName("Número de fichas");
+        graficaTotales.getData().add(serieTotales);
+    }
+
+    public void inicializarGraficaTiempos() {
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("Orden de llegada");
+        NumberAxis y = new NumberAxis(0, 20, 2);
+        y.setLabel("Tiempo en el sistema");
+
+        graficaTiempos = new BarChart<>(x, y);
+        graficaTiempos.setTitle("Tiempo en sistema");
+
+        serieTiempos = new XYChart.Series<>();
+        serieTiempos.setName("Tiempo por ficha");
+        graficaTiempos.getData().add(serieTiempos);
+    }
+
+    public void inicializarGraficoActividad(){
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("Número de turno");
+        NumberAxis y = new NumberAxis();
+        y.setLabel("Valor");
+
+        graficaActividad = new BarChart<>(x, y);
+        graficaActividad.setTitle("Actividad");
+        todasSeries = new ArrayList<>();
+
+        selector = new ComboBox<>();
+        selector.getItems().add("Todos");
+        for(int i = 1; i <= 10; i++){
+            selector.getItems().add("Persona " + i);
+            XYChart.Series<String, Number> serie = new XYChart.Series<>();
+            serie.setName("P" + i);
+            todasSeries.add(serie);
+        }
+        XYChart.Series<String, Number> serieGrupo = new XYChart.Series<>();
+        serieGrupo.setName("Todos");
+        todasSeries.add(serieGrupo);
+
+        selector.setValue("Todos");
+        selector.setOnAction(e -> actualizarSeries());
+        graficaActividad.getData().add(todasSeries.get(10));
+
+        VBox graficaA = new VBox(5, selector, graficaActividad);
+        contenedor.add(graficaA,1, 2);
+    }
+
+    public void actualizarGrafica(int numFichas, int totalFichas, ArrayList<Ficha> fichasP){
+        turnos++;
+        String t = String.valueOf(turnos);
+        serieSalidas.getData().add(new XYChart.Data<>(t, numFichas));
+        serieTotales.getData().add(new XYChart.Data<>(t, totalFichas));
+
+        for (Ficha f : fichasP) {
+            String idFicha = String.valueOf(fichasP.indexOf(f) + 1);
+            serieTiempos.getData().add(new XYChart.Data<>(idFicha, f.getTurnos()));
+        }
+    }
+
+    public void actualizarGraficaActividad(int turno, int[] valoresDado){
+        int sumatoria = 0;
+        String t = String.valueOf(turno);
+        for(int i = 0; i < 10; i++){
+            int valor = valoresDado[i];
+            todasSeries.get(i).getData().add(new XYChart.Data<>(t, valor));
+            sumatoria += valor;
+        }
+        todasSeries.get(10).getData().add(new XYChart.Data<>(t, sumatoria));
+    }
+
+    public void actualizarSeries(){
+        graficaActividad.getData().clear();
+        int idx = selector.getSelectionModel().getSelectedIndex();
+        if(idx == 0){
+            graficaActividad.getData().add(todasSeries.get(10));
+        } else if (idx > 0) {
+            graficaActividad.getData().add(todasSeries.get(idx - 1));
+        }
+    }
+
+    public GridPane getVista(){
+        return contenedor;
+    }
+}
